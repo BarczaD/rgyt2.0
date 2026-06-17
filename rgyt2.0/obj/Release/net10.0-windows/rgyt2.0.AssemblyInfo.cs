1c76ad2d00e0760e592ce4d8e6c0709fb05286ff
BarczaD <barcza.d1@gmail.com>
2026-06-17 09:52:29 +0200

    changed version number and added gitignore

--- rgyt2.0/obj/Release/net10.0-windows/rgyt2.0.AssemblyInfo.cs
+++ rgyt2.0/obj/Release/net10.0-windows/rgyt2.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rgyt2.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e69282770bafd4b81f8130bff1ff267e5e91975")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fd18624d94901a31efe3abb574bf39c9e7a8218")]
 [assembly: System.Reflection.AssemblyProductAttribute("rgyt2.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rgyt2.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
